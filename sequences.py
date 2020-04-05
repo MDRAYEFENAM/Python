84d6372420df8b03c5174e47845a90c025db5c17
Md. Rayef Enam <rayefcse34@gmail.com>
2020-04-05 21:42:33 +0600

    sequences string, list
    Run this file: name[0], name[1], 
    coordinates[0], coordinates[1], names[0], names[1], names[2]

---
+++ sequences.py
@@ -0,0 +1,8 @@
+name = "Alice"
+coordinates = (10.0, 20.0)
+names = ["Alice","Bob", "Charlie"]   ; 
+
+
+
+Run this file: name[0], name[1], 
+coordinates[0], coordinates[1], names[0], names[1], names[2]
